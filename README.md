b7a80d535b5b0c44eca96c1f070337bf61cdc05a
Heathcliff <dennis.nathan@gmail.com>
2018-08-29 12:43:43 -0600

    Added README, set up routes to add a new user and get all users.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Green Light
+
+Green light is a project that I'm working on with a friend that will allow users to turn on a 'green light' on their phone, letting anyone else in the area know that they're interested in talking and hanging out! This specific repo is the back end for that project, utilizing a neo4j database.
--- server.py
+++ server.py
@@ -1,21 +1,49 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g, Response
 from flask_cors import CORS
+from json import dumps
+
+from neo4j.v1 import GraphDatabase
+
+uri = "bolt://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "1234"))
 
 app = Flask(__name__)
 CORS(app)
 
-fake_data = {
-    'picture': 'https://via.placeholder.com/350x350',
-    'name': 'Hingle McCringleberry',
-    'email': 'hipthrust69@gmail.com'
-}
+def get_db():
+    if not hasattr(g, 'neo4j_db'):
+        g.neo4j_db = driver.session()
+    return g.neo4j_db
 
 @app.route('/', methods=['GET'])
 def basic_route():
     return 'It works.'
 
-@app.route('/userdata', methods=['GET'])
-def get_user_data():
-    return jsonify(fake_data)
+@app.route('/newuser/<name>', methods=['POST'])
+def new_user(name):
+    db = get_db()
+    queries = (
+        '''
+        CREATE (le:Person {name:$name})
+        RETURN le.name
+        '''
+                )
+    results = db.run(queries, name=name)
+    records = []
+    for result in results:
+        records.append({"name": result["le.name"]})
+    return 'Successfully added {} to the database.'.format(records[0]['name'])
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    db = get_db()
+    results = db.run("MATCH (name:Person)"
+             "RETURN name.name")
+    records = []
+    for record in results:
+        print(record)
+        records.append({"name": record["name.name"]})
+    return jsonify(records)
+
 
 app.run(host='0.0.0.0', port=5000)
